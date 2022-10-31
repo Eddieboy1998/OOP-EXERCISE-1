67e93e6fb882a8e77daaaa625fd4faf1e3ef2820
Eddie Boy Ranes <dhie211998@gmail.com>
2022-10-31 08:38:12 +0800

    change print to Hello World

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main
 	public static void main(String[] args)
 	{
 	
-		System.out.println("Hi World");
+		System.out.println("Hello World");
 
 		Scanner input = new Scanner(System.in);
 
